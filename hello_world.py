3210bb4fb24e735f6f8532f54ed6afee608586bd
Cameron Carter <cameron.carter@mths.ca>
2021-04-23 13:58:35 +0000

    made hello world program

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Cameron Carter
+# Created on April 2021
+# This is a simple "Hello, World!" program with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
